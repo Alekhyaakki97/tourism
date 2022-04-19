cd3a137b17a4274489a2311a2230f8998d9ac6ff
Alekhyaakki97 <103600459+Alekhyaakki97@users.noreply.github.com>
2022-04-19 01:15:50 -0400

    Add files via upload

---
+++ cust-feedback.php
@@ -0,0 +1,152 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['login'])==0)
+	{	
+header('location:index.php');
+}
+else{
+$fid=intval($_GET['fid']);
+if(isset($_POST['submit5']))
+	{
+$useremail=$_POST['useremail'];
+$drating=$_POST['driverrating'];
+$feedback=$_POST['feedback'];	
+$sql="INSERT INTO custfeedback(BookingId,UserEmail,DriverRating,Feedback) VALUES(:fid,:useremail,:drating,:feedback)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':useremail',$useremail,PDO::PARAM_STR);
+$query->bindParam(':fid',$fid,PDO::PARAM_STR);
+$query->bindParam(':drating',$drating,PDO::PARAM_STR);
+$query->bindParam(':feedback',$feedback,PDO::PARAM_STR);
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+$error="Something went wrong. Please try again";
+}
+else 
+{
+$msg="feedback Submitted Successfully";
+}
+}
+}
+
+	?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Users Feedback</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Tourism Management System In PHP" />
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+  <style>
+		.errorWrap {
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #dd3d36;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+.succWrap{
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #5cb85c;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+		</style>
+</head>
+<body>
+<!-- top-header -->
+<div class="top-header">
+<?php include('includes/header.php');?>
+<div class="banner-1 ">
+	<div class="container">
+		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">Efficient Tour Planning System</h1>
+	</div>
+</div>
+<!--- /banner-1 ---->
+<!--- privacy ---->
+<div class="privacy">
+	<div class="container">
+		<h3 class="wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">Feedback</h3>
+		<form name="chngpwd" method="post">
+		 <?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
+				else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
+<?php
+$fid=intval($_GET['fid']);
+$sql ="SELECT * FROM tblbooking WHERE BookingId=:fid";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':fid', $fid, PDO::PARAM_STR);	
+$query-> execute();
+$results = $query -> fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{ ?>
+
+<p style="width: 350px;">
+<b>Booking Id</b> 
+<input type="text" name="fid" class="form-control" placeholder="Booking Id" value="<?php echo htmlentities($result->BookingId);?>" readonly="">
+			</p> 
+
+<?php $useremail=$_SESSION['login']; ?>
+<p style="width: 350px;">
+<b>Email</b> 
+<input type="email" name="useremail" class="form-control" value="<?php echo htmlentities($useremail);?>" readonly="">
+			</p>
+
+<p style="width: 350px;">
+<b>Driver Rating</b>
+	<input type="text" class="form-control" name="driverrating" placeholder="Driver rating (1-5)" required="">
+			</p>
+
+<p style="width: 350px;">
+<b>Feedback</b>
+	<input type="text" class="form-control" name="feedback" placeholder="Give your feedback" required="">
+			</p>
+
+<?php } ?>
+
+			<p style="width: 350px;">
+<button type="submit" name="submit5" class="btn-primary btn">Submit</button>
+			</p>
+			</form>
+
+		
+	</div>
+</div>
+<!--- /privacy ---->
+<!--- footer-top ---->
+<!--- /footer-top ---->
+<!--?php include('includes/footer.php');?-->
+<!-- signup -->
+<?php include('includes/signup.php');?>			
+<!-- //signu -->
+<!-- signin -->
+<?php include('includes/signin.php');?>			
+<!-- //signin -->
+<!-- write us -->
+<?php include('includes/write-us.php');?>
+</body>
+</html>
+<?php } ?>
---
+++ custom-tour-history.php
@@ -0,0 +1,214 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['login'])==0)
+	{	
+header('location:index.php');
+}
+else{
+if(isset($_REQUEST['bkid']))
+	{
+		$bid=intval($_GET['bkid']);
+$email=$_SESSION['login'];
+	$sql ="SELECT FromDate FROM tblcustpackages WHERE UserEmail=:email and BookingId=:bid";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':email', $email, PDO::PARAM_STR);
+$query-> bindParam(':bid', $bid, PDO::PARAM_STR);
+$query-> execute();
+$results = $query -> fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{
+	 $fdate=$result->FromDate;
+
+	$a=explode("/",$fdate);
+	$val=array_reverse($a);
+	 $mydate =implode("/",$val);
+	$cdate=date('Y/m/d');
+	$date1=date_create("$cdate");
+	$date2=date_create("$fdate");
+ $diff=date_diff($date1,$date2);
+echo $df=$diff->format("%a");
+if($df>1)
+{
+$status=2;
+$cancelby='u';
+$sql = "UPDATE tblcustpackages SET status=:status,CancelledBy=:cancelby WHERE UserEmail=:email and BookingId=:bid";
+$query = $dbh->prepare($sql);
+$query -> bindParam(':status',$status, PDO::PARAM_STR);
+$query -> bindParam(':cancelby',$cancelby , PDO::PARAM_STR);
+$query-> bindParam(':email',$email, PDO::PARAM_STR);
+$query-> bindParam(':bid',$bid, PDO::PARAM_STR);
+$query -> execute();
+
+$msg="Booking Cancelled successfully";
+}
+else
+{
+$error="You can't cancel booking before 24 hours";
+}
+}
+}
+}
+
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Efficient Tour Planning System</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Tourism Management System In PHP" />
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+
+  <style>
+		.errorWrap {
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #dd3d36;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+.succWrap{
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #5cb85c;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+		</style>
+</head>
+<body>
+<!-- top-header -->
+<div class="top-header">
+<?php include('includes/header.php');?>
+<div class="banner-1 ">
+	<div class="container">
+		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">Efficient Tour Planning System</h1>
+	</div>
+</div>
+<!--- /banner-1 ---->
+<!--- privacy ---->
+<div class="privacy">
+	<div class="container">
+		<h3 class="wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">My Custom Tours</h3>
+		<form name="chngpwd" method="post" onSubmit="return valid();">
+		 <?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
+				else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
+	<p>
+	<table border="1" width="100%">
+<tr align="center">
+<th>#</th>
+<th>Booking Id</th>
+<th>From</th>	
+<th>Destination</th>
+<th>Between Places</th>
+<th>Price</th>
+<th>From Date</th>
+<th>To Date</th>
+<th>Vehicle</th>
+<th>Members</th>
+<th>Status</th>
+<th>Action</th>
+</tr>
+<?php 
+
+$uemail=$_SESSION['login'];
+$sql = "SELECT * FROM tblcustpackages where UserEmail=:uemail";
+$query = $dbh->prepare($sql);
+$query -> bindParam(':uemail', $uemail, PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{	?>
+<tr align="center">
+<td><?php echo htmlentities($cnt);?></td>
+<td><?php echo htmlentities($result->BookingId);?></td>
+<td><?php echo htmlentities($result->Start);?></a></td>
+<td><?php echo htmlentities($result->Destination);?></td>
+<td><?php echo htmlentities($result->BetweenPlaces);?></td>
+<td><?php echo htmlentities($result->PackagePrice);?></td>
+<td><?php echo htmlentities($result->FromDate);?></td>
+<td><?php echo htmlentities($result->ToDate);?></td>
+<td><?php echo htmlentities($result->Vehicle);?></td>
+<td><?php echo htmlentities($result->Members);?></td>
+<td><?php if($result->status==0)
+{
+echo "Pending";
+}
+if($result->status==1)
+{
+echo "Confirmed";
+}
+if($result->status==3)
+{
+echo "Completed";
+}
+if($result->status==2 and  $result->CancelledBy=='u')
+{
+echo "Canceled by you ";
+} 
+if($result->status==2 and $result->CancelledBy=='a')
+{
+echo "Canceled by admin ";
+
+}
+?></td>
+<?php if($result->status==1)
+{
+	?><td>Confirmed</td>
+<?php } else if($result->status==2) {
+	?><td>Cancelled</td>
+<?php } else if($result->status==3) {
+	?><td><a href="cust-feedback.php?fid=<?php echo htmlentities($result->BookingId);?>" >Feedback</a></td>
+<?php } else {?>
+<td><a href="custom-tour-history.php?bkid=<?php echo htmlentities($result->BookingId);?>" onclick="return confirm('Do you really want to cancel booking')" >Cancel</a></td>
+<?php }?>
+</tr>
+<?php $cnt=$cnt+1; }} ?>
+	</table>
+		
+			</p>
+			</form>
+
+		
+	</div>
+</div>
+<!--- /privacy ---->
+
+<!--- footer-top ---->
+<!--- /footer-top ---->
+<!--?php include('includes/footer.php');?-->
+<!-- signup -->
+<?php include('includes/signup.php');?>			
+<!-- //signu -->
+<!-- signin -->
+<?php include('includes/signin.php');?>			
+<!-- //signin -->
+<!-- write us -->
+<?php include('includes/write-us.php');?>
+</body>
+</html>
+<?php } ?>

b217991f13bf355441c1f1a4321d8e6eb2f753d1
Alekhyaakki97 <103600459+Alekhyaakki97@users.noreply.github.com>
2022-05-16 23:15:02 -0400

    Add files via upload

---
+++ tagency/includes/sidebarmenu.php
@@ -0,0 +1,26 @@
+<div class="sidebar-menu">
+					<header class="logo1">
+						<a href="#" class="sidebar-icon"> <span class="fa fa-bars"></span> </a> 
+					</header>
+						<div style="border-top:1px ridge rgba(255, 255, 255, 0.15)"></div>
+                           <div class="menu">
+									<ul id="menu" >
+										<li><a href="dashboard.php"><i class="fa fa-tachometer"></i> <span>Home</span><div class="clearfix"></div></a></li>
+										
+									 <li id="menu-academico" ><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i><span> Tour Packages</span> <span class="fa fa-angle-right" style="float: right"></span><div class="clearfix"></div></a>
+										   <ul id="menu-academico-sub" >
+										   <li id="menu-academico-avaliacoes" ><a href="view-packages.php">Regular</a></li>
+											<li id="menu-academico-avaliacoes" ><a href="custom-packages.php">Custom</a></li>
+										  </ul>
+										</li>
+									<li id="menu-academico" ><a href="view-users.php"><i class="fa fa-users" aria-hidden="true"></i><span> Users</span><div class="clearfix"></div></a></li>
+									
+									<li><a href="view-bookings.php"><i class="fa fa-list" aria-hidden="true"></i>  <span> Bookings</span><div class="clearfix"></div></a></li>
+									 <li><a href="view-feedback.php"><i class="fa fa-list"></i>  <span>Feedbacks</span><div class="clearfix"></div></a></li>
+									<li><a href="view-cust-feedback.php"><i class="fa fa-list" aria-hidden="true"></i>  <span>Custom Tour Feedbacks</span><div class="clearfix"></div></a></li>
+									<!--li><a href="manage-pages.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>  <span>Manage Pages</span><div class="clearfix"></div></a></li-->
+							     
+									
+								  </ul>
+								</div>
+							  </div>
